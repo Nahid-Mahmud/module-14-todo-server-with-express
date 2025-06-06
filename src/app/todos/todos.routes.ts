c3b385781622f0299eeec69f80f8ab6d3e4dca1c
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-06 21:42:59 +0600

    feat: refactor todos routes to use MongoDB for data storage and retrieval

--- src/app/todos/todos.routes.ts
+++ src/app/todos/todos.routes.ts
@@ -2,14 +2,19 @@ import express, { Request, Response } from "express";
 import fs from "fs";
 import path from "path";
 import { client } from "../../config/mongodb";
+import { ObjectId } from "mongodb";
 
 const todosRouter = express.Router();
 
 const filePath = path.join(__dirname, "../../../src/db/todo.json");
 
-todosRouter.get("/", (req: Request, res: Response) => {
-  const data = fs.readFileSync(filePath, "utf-8");
-  const todos = JSON.parse(data);
+todosRouter.get("/", async (req: Request, res: Response) => {
+  const db = client.db("todoDb");
+  const collection = db.collection("todos");
+
+  const todos = await collection.find({}).toArray();
+
+  console.log(todos);
 
   res.json({
     todos,
@@ -18,65 +23,64 @@ todosRouter.get("/", (req: Request, res: Response) => {
 });
 
 todosRouter.post("/create-todo", async (req: Request, res: Response) => {
-  // const { title, body } = req.body;
+  const { title, description, priority } = req.body;
   // console.log(title);
   const db = await client.db("todoDb");
 
   const collection = db.collection("todos");
 
   await collection.insertOne({
-    title: "Mongodb",
-    description: "Learning Mongodb",
-    priority: "High",
+    title,
+    description,
+    priority,
     isCompleted: false,
+    createdAt: new Date().toISOString(),
   });
 
-  const todos = collection.find({}).toArray();
-
   //   res.send("Hello, World!");
-  res.status(201).json(todos);
+  res
+    .status(201)
+    .json({ message: "Todo created successfully", todo: { title, description, priority, isCompleted: false } });
 });
 
-todosRouter.get("/:id", (req: Request, res: Response) => {
-  const allTodos = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+todosRouter.get("/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const db = await client.db("todoDb");
+  const collection = await db.collection("todos");
 
-  const requestedTodoId = req.params.id;
-
-  const todo = allTodos.find((t: { id: string }) => t.id === requestedTodoId);
+  const todo = await collection.findOne({
+    _id: new ObjectId(id),
+  });
 
   if (!todo) {
     res.status(404).json({ message: "Todo not found" });
   }
 
-  res.status(200).json(todo);
+  res.status(200).json({ todo, message: "Todo fetched successfully" });
 });
+
+// update todo
 
 todosRouter.patch("/:id", (req: Request, res: Response) => {
   const id = req.params.id;
-  const { title } = req.body;
-  const allTodos = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-
-  const todo = allTodos.find((t: { id: string }) => t.id === id);
-  if (!todo) {
-    res.status(404).json({ message: "Todo not found" });
-  }
-  todo.title = title;
-  fs.writeFileSync(filePath, JSON.stringify(allTodos, null, 4), "utf-8");
-  res.status(200).json({ message: "Todo updated successfully", todo });
 });
 
-todosRouter.delete("/:id", (req: Request, res: Response) => {
+todosRouter.delete("/:id", async (req: Request, res: Response) => {
   const id = req.params.id;
-  const allTodos = JSON.parse(fs.readFileSync(filePath, "utf-8"));
-  const findTodo = allTodos.find((t: { id: string }) => t.id === id);
-  if (!findTodo) {
+  const db = await client.db("todoDb");
+  const collection = await db.collection("todos");
+  const result = await collection.deleteOne({
+    _id: new ObjectId(id),
+  });
+
+  if (result.deletedCount === 0) {
     res.status(404).json({ message: "Todo not found" });
   }
-  const todoWithoutId = allTodos.filter((t: { id: string }) => t.id !== id);
 
-  fs.writeFileSync(filePath, JSON.stringify(todoWithoutId, null, 4), "utf-8");
-
-  res.status(200).json({ message: "Todo deleted successfully" });
+  res.status(200).json({
+    message: "Todo deleted successfully",
+    result,
+  });
 });
 
 export default todosRouter;
