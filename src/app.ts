e042a1a1d2549ebd1c540ae3fe38e2584b8fd0cb
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-09 19:48:44 +0600

    feat: update custom logger middleware to use NextFunction type for improved type safety

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 const app: Application = express();
 import fs from "fs";
 import path from "path";
@@ -6,16 +6,17 @@ import todosRouter from "./app/todos/todos.routes";
 
 app.use(express.json());
 
-const customLogger = (req: Request, res: Response, next: Function) => {
+const customLogger = (req: Request, res: Response, next: NextFunction) => {
   const logMessage = `${new Date().toISOString()} - ${req.method} ${req.originalUrl}`;
   console.log(logMessage);
 
   next();
 };
+app.use(customLogger);
 
 app.use("/todos", todosRouter);
 
-app.get("/", customLogger, (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   // console.log(req, res);
   res.send("Welcome to the Todo API!");
 });
