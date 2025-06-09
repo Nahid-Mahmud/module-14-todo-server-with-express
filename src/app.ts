8aa5f53e277547d67c0b99892edbe4272c5baae3
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-06-09 20:56:22 +0600

    feat: add error handling routes and global error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -23,4 +23,31 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to the Todo API!");
 });
 
+app.get("/error", async (req: Request, res: Response, next: NextFunction) => {
+  // Simulating an error
+  // console.log(first);
+
+  try {
+    // console.log(first);
+  } catch (error) {
+    // console.error("An error occurred:", error);
+    next(error);
+  }
+
+  res.status(500).json({ message: "This is a simulated error." });
+});
+
+// not found handler
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+  if (error) {
+    console.log(error);
+    res.status(500).json({ message: "An error occurred , Message form global error handler", error: error.message });
+  }
+});
+
 export default app;
